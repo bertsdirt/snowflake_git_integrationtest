69a369786ea474d29bdd332988fcd8efb57c57da
Kevin Bert <kevin@kevinbert.com>
2023-07-27 14:20:19 -0500

    init

---
+++ app.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from snowflake.snowpark.session import Session
+from snowflake.snowpark import DataFrame
+from snowflake.snowpark.functions import col
+
+def hello(session: Session) -> DataFrame:
+    df = session.table("SFDB_SBOX.DSANDD01.KB_PRICE_COND")
+    return df;
+# return df.filter(col("SAP_SLS_ORG_CD") == 2000)
+
+# For local debugging
+if __main__ == "__main__":
+    from snowflake.snowpark.mock.mock_connection import MockServerConnection
+    import init_local
+
+    session = Session(MockServerConnection())
+    session = init_local.init(session)
+    print(hello(session).show())
+
